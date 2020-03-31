7fd3d7582e3d1b592a677f8e121a5fc2c0661a33
Arch-Kali <55939094+Arch-Kali@users.noreply.github.com>
2020-03-31 09:49:59 +0000

    Update necklace.py

--- necklace.py
+++ necklace.py
@@ -29,9 +29,27 @@ def sameNecklace(word1, word2):
 
 
 
-list = ['lutelyabso', 'ssibleacce', 'vitiesacti', 'tionaladdi', 'rdableaffo', 'rentlyappa', 'aranceappe', 'icableappl', 'icantsappl', 'eciateappr', 'ssmentasse', 'stanceassi', 'ciatedasso', 'sphereatmo', 'activeattr', 'groundback', 'etballbask', 'nessesbusi', 'idatescand', 'goriescate', 'lengeschal', 'acterschar', 'ectioncoll', 'ercialcomm', 'issioncomm', 'itmentcomm', 'arisoncomp', 'atiblecomp', 'letelycomp', 'letioncomp', 'liancecomp', 'onentscomp', 'lusionconc', 'itionscond', 'erenceconf', 'idenceconf', 'ectionconn', 'ideredcons', 'istentcons', 'tantlycons', 'ainingcont', 'inuouscont', 'ributecont', 'rollercont', 'enientconv', 'ersionconv', 'iculumcurr', 'nitelydefi', 'nitiondefi', 'rtmentdepa', 'eduresproc']
+list = ['lutelyabso', 'ssibleacce', 'vitiesacti', 'tionaladdi', 'rdableaffo', 
+	'rentlyappa', 'aranceappe', 'icableappl', 'icantsappl', 'eciateappr', 
+	'ssmentasse', 'stanceassi', 'ciatedasso', 'sphereatmo', 'activeattr', 
+	'groundback', 'etballbask', 'nessesbusi', 'idatescand', 'goriescate', 
+	'lengeschal', 'acterschar', 'ectioncoll', 'ercialcomm', 'issioncomm', 
+	'itmentcomm', 'arisoncomp', 'atiblecomp', 'letelycomp', 'letioncomp', 
+	'liancecomp', 'onentscomp', 'lusionconc', 'itionscond', 'erenceconf', 
+	'idenceconf', 'ectionconn', 'ideredcons', 'istentcons', 'tantlycons', 
+	'ainingcont', 'inuouscont', 'ributecont', 'rollercont', 'enientconv', 
+	'ersionconv', 'iculumcurr', 'nitelydefi', 'nitiondefi', 'rtmentdepa', 'eduresproc']
 
-origin = ['absolutely', 'accessible', 'activities', 'additional', 'affordable', 'apparently', 'appearance', 'applicable', 'applicants', 'appreciate', 'assessment', 'assistance', 'associated', 'atmosphere', 'attractive', 'background', 'basketball', 'businesses', 'candidates', 'categories', 'challenges', 'characters', 'collection', 'commercial', 'commission', 'commitment', 'comparison', 'compatible', 'completely', 'completion', 'compliance', 'components', 'conclusion', 'conditions', 'conference', 'confidence', 'connection', 'considered', 'consistent', 'constantly', 'containing', 'continuous', 'contribute', 'controller', 'convenient', 'conversion', 'curriculum', 'definitely', 'definition', 'department', 'procedures']
+origin = ['absolutely', 'accessible', 'activities', 'additional', 'affordable', 
+	  'apparently', 'appearance', 'applicable', 'applicants', 'appreciate', 
+	  'assessment', 'assistance', 'associated', 'atmosphere', 'attractive', 
+	  'background', 'basketball', 'businesses', 'candidates', 'categories', 
+	  'challenges', 'characters', 'collection', 'commercial', 'commission', 
+	  'commitment', 'comparison', 'compatible', 'completely', 'completion', 
+	  'compliance', 'components', 'conclusion', 'conditions', 'conference', 
+	  'confidence', 'connection', 'considered', 'consistent', 'constantly', 
+	  'containing', 'continuous', 'contribute', 'controller', 'convenient', 
+	  'conversion', 'curriculum', 'definitely', 'definition', 'department', 'procedures']
 
 
 for i in range(len(origin)):
